fc11001f171151fbe9e91c653f34b5b9e05bc4f2
Elekes Tamás <elekestamas@protonmail.com>
2017-06-15 18:49:56 +0200

    initial commit

---
+++ util.py
@@ -0,0 +1,34 @@
+import random
+
+
+def get_random_base():
+    baseint = random.randint(0, 99)
+
+    if baseint < 40:
+        return 'C'
+    elif baseint < 50:
+        return 'T'
+    elif baseint < 80:
+        return 'G'
+    else:
+        return 'A'
+
+
+def get_base_pair(base):
+    if base == 'C':
+        return 'G'
+    elif base == 'T':
+        return 'A'
+    elif base == 'G':
+        return 'C'
+    else:
+        return 'T'
+
+
+def search_pattern(sense, pattern):
+    for i in range(0, len(sense) - len(pattern) + 1):
+        j = 0
+        while j < len(pattern) and pattern[j] == sense[i + j] :
+            j += 1
+        if j == len(pattern):
+            print("HEUREKA")
