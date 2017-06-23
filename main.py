2078fce39d1db4e2fb779e4b3c08ed12c0917769
Tamas Elekes <tamas.elekes@commsignia.com>
2017-06-23 10:28:12 +0200

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from util import get_random_base, get_base_pair, search_pattern
 
 sense = []
-for i in range(0, 50):
+for i in range(0, 500):
     base = get_random_base()
     pair = get_base_pair(base)
     sense.append((base, pair))
 
-pattern = [ sense[len(sense)-3], sense[len(sense)-2], sense[len(sense)-1]]
+pattern = [('G', 'C'), ('G', 'C'), ('A', 'T')]
 print(sense)
 print(pattern)
 search_pattern(sense, pattern)
